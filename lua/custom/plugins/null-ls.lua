74ec640f59673b097f2368c7897b489a4ee2ee80
Yannick Spies <yannickspies91@gmail.com>
2024-10-01 16:09:05 +0200

    finally starting to look like something with vue

---
+++ lua/custom/plugins/null-ls.lua
@@ -0,0 +1,23 @@
+return {
+  'nvimtools/none-ls.nvim',
+  config = function()
+    local null_ls = require 'null-ls'
+
+    null_ls.setup {
+      sources = {
+        null_ls.builtins.formatting.prettier.with {
+          filetypes = { 'javascript', 'typescript', 'vue', 'json', 'html', 'css', 'markdown' }, -- Filetypes to format
+        },
+      },
+    }
+
+    -- Autoformat on save
+    vim.cmd [[
+      augroup FormatAutogroup
+        autocmd!
+        autocmd BufWritePost *.js,*.ts,*.vue,*.json,*.html,*.css,*.md lua vim.lsp.buf.format({ async = true })
+      augroup END
+    ]]
+  end,
+  requires = { 'nvim-lua/plenary.nvim' },
+}
