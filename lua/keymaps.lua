f4d7d6eb6bbfbcb474535efb0e725755fc694828
Yannick Spies <yannickspies91@gmail.com>
2024-10-01 15:39:00 +0200

    added keymaps and some options

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,5 +1,6 @@
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
+local opts = { noremap = true, silent = true }
 
 -- Clear highlights on search when pressing <Esc> in normal mode
 --  See `:help hlsearch`
@@ -45,4 +46,14 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+-- Select all
+vim.keymap.set('n', '<C-a>', 'gg<S-v>G')
+
+-- Disable continuations
+vim.keymap.set('n', '<Leader>o', 'o<Esc>^Da', opts)
+vim.keymap.set('n', '<Leader>O', 'O<Esc>^Da', opts)
+
+-- Save file
+vim.keymap.set('n', '<C-s>', ':w<CR>', opts)
+vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>a', opts)
 -- vim: ts=2 sts=2 sw=2 et
