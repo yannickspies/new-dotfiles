e19226485d355bc48b1be2b984eade5aec3473e0
Yannick Spies <yannickspies91@gmail.com>
2024-10-02 20:15:52 +0200

    added noice and formatting for lua to null-ls

--- lua/custom/plugins/null-ls.lua
+++ lua/custom/plugins/null-ls.lua
@@ -6,16 +6,17 @@ return {
     null_ls.setup {
       sources = {
         null_ls.builtins.formatting.prettier.with {
-          filetypes = { 'javascript', 'typescript', 'vue', 'json', 'html', 'css', 'markdown',  'lua' }, -- Filetypes to format
+          filetypes = { 'javascript', 'typescript', 'vue', 'json', 'html', 'css', 'markdown' },
         },
+        -- Stylua for Lua
+        null_ls.builtins.formatting.stylua,
       },
     }
 
-    -- Autoformat on save
     vim.cmd [[
       augroup FormatAutogroup
         autocmd!
-        autocmd BufWritePost *.js,*.ts,*.vue,*.json,*.html,*.css,*.md lua vim.lsp.buf.format({ async = true })
+        autocmd BufWritePost *.js,*.ts,*.vue,*.json,*.html,*.css,*.md,*.lua lua vim.lsp.buf.format({ async = true })
       augroup END
     ]]
   end,
